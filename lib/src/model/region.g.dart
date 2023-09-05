9453e53a45bf33fc902cbf95d00bef221c785a72
MarxLaw <96363263+MarxLaw@users.noreply.github.com>
2023-09-05 14:03:08 +0800

    Update region.g.dart

--- lib/src/model/region.g.dart
+++ lib/src/model/region.g.dart
@@ -9,9 +9,9 @@ part of 'region.dart';
 _$_Region _$$_RegionFromJson(Map<String, dynamic> json) => _$_Region(
       id: json['id'] as String,
       regionName: json['regionName'] as String,
-      provinces: (json['provinces'] as List<dynamic>)
-          .map((e) => Province.fromJson(e as Map<String, dynamic>))
-          .toList(),
+      // provinces: (json['provinces'] as List<dynamic>)
+      //     .map((e) => Province.fromJson(e as Map<String, dynamic>))
+      //     .toList(),
     );
 
 Map<String, dynamic> _$$_RegionToJson(_$_Region instance) => <String, dynamic>{
